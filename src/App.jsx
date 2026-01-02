8a729c17bd28edcd5e3db7f116a89c93bf1009c5
ryder <rudrchattrjee@gmail.com>
2026-01-02 05:39:08 +0530

    second

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='w-full h-screen flex justify-center items-center bg-black text-4xl text-white'>
+      Hello this website is hosted in cloud.
+    </div>
+    </>
+  )
+}
+
+export default App
